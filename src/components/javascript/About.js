8f6ca88d3512d8eeff10a9eb480a331a8d2e1ebe
20JINN <leeyj2346@gmail.com>
2018-10-08 21:33:29 +0900

    [about] layout

--- src/components/javascript/About.js
+++ src/components/javascript/About.js
@@ -1,11 +1,38 @@
 import React, { Component } from 'react';
 import '../scss/About.scss';
+import mainVideo from '../resource/video/main.mp4';
 
 class About extends Component {
   render() {
     return (
       <div className="About">
-        <h1>This is About!</h1>
+        <div className="section1 marginContainer">
+          <h1 className="b">Make your video more dynamic, dramatic <br></br>and divers</h1>
+          <div className="imgContainer">
+            <div className="img1"></div>
+            <div className="img2"></div>
+          </div>
+        </div>
+        <div className="section2">
+          <div className="videoSize">
+            <div className="videoLayer">
+              <video autoPlay loop muted>
+                <source src={mainVideo} type="video/mp4" />
+                Your browser does not support the video tag.
+              </video>
+            </div>
+          </div>
+        </div>
+        <div className="section3 marginContainer">
+          <h1 className="b">Services</h1>
+          <p className="l">Make your video more dynamic, dramatic 
+  and diversMake your video more dynamic, dramatic 
+  and diversMake your video more dynamic, dramatic 
+  and diversMake your video more dynamic, dramatic 
+  and divers</p>
+        </div>
+
+
       </div>
     );
   }
